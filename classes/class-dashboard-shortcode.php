f597a77e939d91201131b11075851b7093317f20
hwaqar <hamzawaqar556@gmail.com>
2025-07-04 11:21:43 +0500

    Add tabs

--- classes/class-dashboard-shortcode.php
+++ classes/class-dashboard-shortcode.php
@@ -10,17 +10,18 @@ class DashboardShortcode
 
     public function render_dashboard()
     {
+        $user = wp_get_current_user();
+
         // Check if user is logged in
-        if ( is_user_logged_in() && current_user_can('administrator') ) {
-    ob_start();
-                hdl_get_template('dashboard/wrapper', ['icons' => hdl_icons(), 'user' => $user]);
-                return ob_get_clean();
-}
+        if (is_user_logged_in() && current_user_can('administrator')) {
+            ob_start();
+            hdl_get_template('dashboard/wrapper', ['icons' => hdl_icons(), 'user' => $user]);
+            return ob_get_clean();
+        }
 
 
 
         if (is_user_logged_in()) {
-            $user = wp_get_current_user();
 
             // Check if user has only 'subscriber' role
             if (in_array('subscriber', (array) $user->roles)) {
