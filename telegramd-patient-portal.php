440e8b0096938876a2e1e4266726adc47d1dfd66
Muhammad Faheem Hassan <faheemh127@gmail.com>
2025-08-31 20:29:47 +0500

    Refactoring further and clean the code

--- telegramd-patient-portal.php
+++ telegramd-patient-portal.php
@@ -9,7 +9,8 @@
  */
 
 
-
+// Constants
+define('TELEGRA_PATIENT_PORTAL_PATH', plugin_dir_path(__FILE__));
 
 add_action('wp_ajax_create_payment_intent', 'my_create_payment_intent');
 add_action('wp_ajax_nopriv_create_payment_intent', 'my_create_payment_intent');
@@ -63,25 +64,28 @@ function my_create_setup_intent()
 
 
 
-define('TELEGRA_PATIENT_PORTAL_PATH', plugin_dir_path(__FILE__));
 require_once __DIR__ . '/vendor/autoload.php';
-include_once(plugin_dir_path(__FILE__) . 'includes/api-keys.php');
-include_once(plugin_dir_path(__FILE__) . 'classes/class-hld-user-orders.php');
-include_once(plugin_dir_path(__FILE__) . 'includes/class-hld-user-notifications.php');
+
 foreach (glob(plugin_dir_path(__FILE__) . 'helper/*.php') as $file) {
     require_once $file;
 }
-require_once TELEGRA_PATIENT_PORTAL_PATH . 'classes/class-hld-assets.php';
+
+// Include All Necessary files
+require_once plugin_dir_path(__FILE__) . 'includes/api-keys.php';
+require_once plugin_dir_path(__FILE__) . 'includes/functions.php';
+require_once plugin_dir_path(__FILE__) . 'classes/class-hld-user-orders.php';
+require_once plugin_dir_path(__FILE__) . 'includes/class-hld-user-notifications.php';
+require_once plugin_dir_path(__FILE__) . 'classes/class-hld-assets.php';
 require_once plugin_dir_path(__FILE__) . 'classes/class-dashboard-shortcode.php';
 require_once plugin_dir_path(__FILE__) . 'classes/class-telegra.php';
-include_once(plugin_dir_path(__FILE__) . 'classes/class-fluent-handler.php');
-include_once('includes/functions.php');
+require_once plugin_dir_path(__FILE__) . 'classes/class-fluent-handler.php';
 require_once plugin_dir_path(__FILE__) . 'ajax/save-payment-method.php';
 require_once plugin_dir_path(__FILE__) . 'ajax/log-payment-success.php';
-include_once('includes/shortcodes.php');
-include_once(plugin_dir_path(__FILE__) . 'includes/order-tracking-webhook.php');
-include_once(plugin_dir_path(__FILE__) . 'includes/prescription-received-webhook.php');
-include_once(plugin_dir_path(__FILE__) . 'includes/hooks.php');
-include_once(plugin_dir_path(__FILE__) . 'ajax/save-form-url.php');
-include_once(plugin_dir_path(__FILE__) . 'includes/patient-login.php');
-require_once TELEGRA_PATIENT_PORTAL_PATH . 'ajax/patient-login.php';
+require_once plugin_dir_path(__FILE__) . 'includes/shortcodes.php';
+require_once plugin_dir_path(__FILE__) . 'includes/order-tracking-webhook.php';
+require_once plugin_dir_path(__FILE__) . 'includes/prescription-received-webhook.php';
+require_once plugin_dir_path(__FILE__) . 'includes/hooks.php';
+require_once plugin_dir_path(__FILE__) . 'ajax/save-form-url.php';
+require_once plugin_dir_path(__FILE__) . 'includes/patient-login.php';
+require_once plugin_dir_path(__FILE__) . 'ajax/patient-login.php';
+
