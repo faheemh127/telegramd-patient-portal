36819e6b956b58754f10e6d1740c76f60a33019b
Abubakar Siddique <a4aboo@yahoo.com>
2025-11-20 18:00:57 +0000

    prefunnel file added, it was left before.

---
+++ ajax/ghl-activate-prefunnel-email.php
@@ -0,0 +1,79 @@
+<?php
+
+add_action('hld_send_ghl_webhook_event', 'hld_ghl_execute_webhook_logic', 10, 2);
+add_action('wp_ajax_activate_reminder', 'hld_ghl_activate_reminder');
+add_action('wp_ajax_nopriv_activate_reminder', 'hld_ghl_activate_reminder');
+// function hld_ghl_activate_reminder()
+// {
+//     global $wpdb;
+//     $table = $wpdb->prefix . 'healsend_subscriptions';
+//
+//     // Ensure user is logged in
+//     if (!is_user_logged_in()) {
+//         wp_send_json_error(['message' => 'You must be logged in to continue.']);
+//         wp_die();
+//     }
+//
+//     $current_user = wp_get_current_user();
+//     $patient_email = $current_user->user_email;
+//
+//
+//     $api_key = 'pit-dcbcc991-8612-49ae-a5ff-31046d43da5b';
+//     try {
+//         $GhlApiClient = new GhlApiClient($api_key);
+//         $data = [
+//             "email" => $patient_email
+//         ];
+//
+//         $GhlApiClient->sendToWebhook('https://services.leadconnectorhq.com/hooks/tqGhhCGePHa1hQkrrOQY/webhook-trigger/6Gq0WiCp523gtFLozsJX', $data);
+//
+//         wp_send_json_success([ 'success' => true ]);
+//     } catch (Exception $e) {
+//         wp_send_json_error(['message' => $e->getMessage()]);
+//     }
+//
+//     wp_die();
+// }
+
+
+function hld_ghl_activate_reminder()
+{
+    if (!is_user_logged_in()) {
+        wp_send_json_error(['message' => 'You must be logged in.']);
+        wp_die();
+    }
+
+    $delay_seconds = 900;
+
+    $current_user = wp_get_current_user();
+    $patient_email = $current_user->user_email;
+
+    $args = [$patient_emaily];
+    $hook_name = 'hld_send_ghl_webhook_event';
+
+    $timestamp = wp_next_scheduled($hook_name, $args);
+    if ($timestamp) {
+        wp_unschedule_event($timestamp, $hook_name, $args);
+    }
+
+    wp_schedule_single_event(time() + $delay_seconds, $hook_name, $args);
+    wp_send_json_success(['success' => true, 'message' => 'Reminder scheduled after 15 minutes.']);
+    wp_die();
+}
+
+function hld_ghl_execute_webhook_logic($patient_email)
+{
+    $api_key = 'pit-dcbcc991-8612-49ae-a5ff-31046d43da5b';
+    try {
+        $GhlApiClient = new GhlApiClient($api_key);
+
+        $data = [
+            "email" => $patient_email
+        ];
+
+        $GhlApiClient->sendToWebhook('https://services.leadconnectorhq.com/hooks/tqGhhCGePHa1hQkrrOQY/webhook-trigger/6Gq0WiCp523gtFLozsJX', $data);
+
+    } catch (Exception $e) {
+        error_log('GHL Webhook Failed: ' . $e->getMessage());
+    }
+}
