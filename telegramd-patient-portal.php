feec9772f2a51dc8415920c40174921b207ebf84
Muhammad Faheem Hassan <faheemh127@gmail.com>
2025-12-15 19:06:15 +0500

    ship ajax and classes to the source folder

--- telegramd-patient-portal.php
+++ telegramd-patient-portal.php
@@ -38,54 +38,52 @@ foreach (glob(plugin_dir_path(__FILE__) . 'helper/*.php') as $file) {
 
 
 require_once plugin_dir_path(__FILE__) . 'includes/functions.php';
-require_once plugin_dir_path(__FILE__) . 'classes/class-discount.php';
+require_once plugin_dir_path(__FILE__) . 'source/classes/class-discount.php';
 require_once plugin_dir_path(__FILE__) . 'includes/hooks.php';
-require_once plugin_dir_path(__FILE__) . 'classes/class-ghl-webhook-client.php';
-require_once plugin_dir_path(__FILE__) . 'classes/class-mail.php';
-require_once plugin_dir_path(__FILE__) . 'classes/class-stripe.php';
-require_once plugin_dir_path(__FILE__) . 'classes/class-telegra-order.php';
-
-require_once plugin_dir_path(__FILE__) . 'ajax/stripe-create-setup-intent.php';
-require_once plugin_dir_path(__FILE__) . 'ajax/stripe-charge-now.php';
+require_once plugin_dir_path(__FILE__) . 'source/classes/class-ghl-webhook-client.php';
+require_once plugin_dir_path(__FILE__) . 'source/classes/class-mail.php';
+require_once plugin_dir_path(__FILE__) . 'source/classes/class-stripe.php';
+require_once plugin_dir_path(__FILE__) . 'source/classes/class-telegra-order.php';
+require_once plugin_dir_path(__FILE__) . 'source/ajax/stripe-create-setup-intent.php';
+require_once plugin_dir_path(__FILE__) . 'source/ajax/stripe-charge-now.php';
 require_once plugin_dir_path(__FILE__) . 'includes/api-keys.php';
 require_once plugin_dir_path(__FILE__) . 'includes/funnel-navbar.php';
 require_once plugin_dir_path(__FILE__) . 'includes/funnel-footer.php';
-require_once plugin_dir_path(__FILE__) . 'classes/class-hld-settings.php';
-
-require_once plugin_dir_path(__FILE__) . 'classes/class-subscriptions.php';
+require_once plugin_dir_path(__FILE__) . 'source/classes/class-hld-settings.php';
+require_once plugin_dir_path(__FILE__) . 'source/classes/class-subscriptions.php';
 require_once plugin_dir_path(__FILE__) . 'includes/class-hld-user-notifications.php';
-require_once plugin_dir_path(__FILE__) . 'classes/class-payments.php';
-require_once plugin_dir_path(__FILE__) . 'classes/class-hld-db-backup-manager.php';
-require_once plugin_dir_path(__FILE__) . 'classes/class-hld-assets.php';
-require_once plugin_dir_path(__FILE__) . 'classes/class-patient.php';
-require_once plugin_dir_path(__FILE__) . 'classes/class-telegra.php';
-require_once plugin_dir_path(__FILE__) . 'classes/class-admin-menu.php';
-require_once plugin_dir_path(__FILE__) . 'classes/class-action-item-manager.php';
-require_once plugin_dir_path(__FILE__) . 'classes/class-fluent-handler.php';
-require_once plugin_dir_path(__FILE__) . 'classes/class-dashboard-shortcode.php';
-require_once plugin_dir_path(__FILE__) . 'classes/class-db-tables.php';
-
-require_once plugin_dir_path(__FILE__) . 'ajax/save-payment-method.php';
-require_once plugin_dir_path(__FILE__) . 'ajax/log-payment-success.php';
+require_once plugin_dir_path(__FILE__) . 'source/classes/class-payments.php';
+require_once plugin_dir_path(__FILE__) . 'source/classes/class-hld-db-backup-manager.php';
+require_once plugin_dir_path(__FILE__) . 'source/classes/class-hld-assets.php';
+require_once plugin_dir_path(__FILE__) . 'source/classes/class-patient.php';
+require_once plugin_dir_path(__FILE__) . 'source/classes/class-telegra.php';
+require_once plugin_dir_path(__FILE__) . 'source/classes/class-admin-menu.php';
+require_once plugin_dir_path(__FILE__) . 'source/classes/class-action-item-manager.php';
+require_once plugin_dir_path(__FILE__) . 'source/classes/class-fluent-handler.php';
+require_once plugin_dir_path(__FILE__) . 'source/classes/class-dashboard-shortcode.php';
+require_once plugin_dir_path(__FILE__) . 'source/classes/class-db-tables.php';
+require_once plugin_dir_path(__FILE__) . 'source/ajax/save-payment-method.php';
+require_once plugin_dir_path(__FILE__) . 'source/ajax/log-payment-success.php';
 require_once plugin_dir_path(__FILE__) . 'includes/shortcodes.php';
 require_once plugin_dir_path(__FILE__) . 'includes/order-tracking-webhook.php';
 require_once plugin_dir_path(__FILE__) . 'includes/prescription-received-webhook.php';
-require_once plugin_dir_path(__FILE__) . 'ajax/save-form-url.php';
+require_once plugin_dir_path(__FILE__) . 'source/ajax/save-form-url.php';
 require_once plugin_dir_path(__FILE__) . 'includes/patient-login.php';
 require_once plugin_dir_path(__FILE__) . 'includes/patient-signup.php';
-require_once plugin_dir_path(__FILE__) . 'ajax/patient-login.php';
-require_once plugin_dir_path(__FILE__) . 'ajax/patient-signup.php';
-require_once plugin_dir_path(__FILE__) . 'ajax/stripe-subscribe-patient.php';
-require_once plugin_dir_path(__FILE__) . 'ajax/stripe-revoke-subscription.php';
-require_once plugin_dir_path(__FILE__) . 'ajax/stripe-get-subscription-price.php';
-require_once plugin_dir_path(__FILE__) . 'ajax/update-patient-detail.php';
-require_once plugin_dir_path(__FILE__) . 'ajax/get-telegra-order.php';
-require_once plugin_dir_path(__FILE__) . 'ajax/upload-id.php';
-require_once plugin_dir_path(__FILE__) . 'ajax/upload-glp-agreement-form.php';
-require_once plugin_dir_path(__FILE__) . 'ajax/request-refund.php';
-require_once plugin_dir_path(__FILE__) . 'ajax/ghl-activate-prefunnel-email.php';
-require_once plugin_dir_path(__FILE__) . 'ajax/hld-cancel-user-card-reminders.php';
-require_once plugin_dir_path(__FILE__) . 'ajax/get-stripe-price-data.php';
+require_once plugin_dir_path(__FILE__) . 'source/ajax/patient-login.php';
+require_once plugin_dir_path(__FILE__) . 'source/ajax/patient-signup.php';
+require_once plugin_dir_path(__FILE__) . 'source/ajax/stripe-subscribe-patient.php';
+require_once plugin_dir_path(__FILE__) . 'source/ajax/stripe-revoke-subscription.php';
+require_once plugin_dir_path(__FILE__) . 'source/ajax/stripe-get-subscription-price.php';
+require_once plugin_dir_path(__FILE__) . 'source/ajax/update-patient-detail.php';
+require_once plugin_dir_path(__FILE__) . 'source/ajax/get-telegra-order.php';
+require_once plugin_dir_path(__FILE__) . 'source/ajax/upload-id.php';
+require_once plugin_dir_path(__FILE__) . 'source/ajax/upload-glp-agreement-form.php';
+require_once plugin_dir_path(__FILE__) . 'source/ajax/request-refund.php';
+require_once plugin_dir_path(__FILE__) . 'source/ajax/ghl-activate-prefunnel-email.php';
+require_once plugin_dir_path(__FILE__) . 'source/ajax/hld-cancel-user-card-reminders.php';
+require_once plugin_dir_path(__FILE__) . 'source/ajax/get-stripe-price-data.php';
+
 
 register_activation_hook(__FILE__, function () {
     HLD_DB_Tables::create_tables();
